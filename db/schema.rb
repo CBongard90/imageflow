dd30ac6fc3605c4df42def517b7fdc6be67a5f7d
Christian Bongard <christian.bongard90@gmail.com>
2021-09-09 18:08:24 +0400

    Revert "added active storage"
    This reverts commit d26b97d32dfceff987858bc0bc59d214925c3dc0.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_09_09_112726) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -39,6 +40,9 @@ ActiveRecord::Schema.define(version: 2021_09_09_112726) do
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
+=======
+ActiveRecord::Schema.define(version: 2021_09_09_075635) do
+>>>>>>> parent of d26b97d (added active storage)
 
   create_table "flows", force: :cascade do |t|
     t.string "title"
@@ -65,6 +69,4 @@ ActiveRecord::Schema.define(version: 2021_09_09_112726) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
